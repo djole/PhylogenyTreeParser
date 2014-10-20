13805add1b5e18f268c30f697398f5d33ec6e3f7
calavera <dorde.grbic@gmail.com>
2014-10-20 13:00:36 +0200

    Implement the parsing of newick trees...
    ... and their likelihoods from the NEXUS file.
    
     Changes to be committed:
    	modified:   ParseNexus.hs

--- ParseNexus.hs
+++ ParseNexus.hs
@@ -3,6 +3,7 @@ import Parsers
 import ParserCandies
 import Data.Char
 import System.Environment
+import System.IO
 
 rubishPlusParser_ :: Parser a -> String -> [(a, String)]
 rubishPlusParser_  p "" = []
@@ -12,9 +13,52 @@ rubishPlusParser_  p cs = case parse p cs of
 
 rubishPlusParser p = Parser (rubishPlusParser_ p)
 
+rubishPlusNewickTree = rubishPlusParser newick_tree
+
 
 main = do
 	args <- getArgs
 	content <- readFile (args !! 0)
-	print $ show content
+	let linesOfFile = lines content
+	writeFile (args !! 1) $ parseNLines linesOfFile
+
+
+parse1Line :: String -> String
+parse1Line = \l -> show $ parseAndPeel parseTreeLine l
+
+parseNLines :: [String] -> String
+parseNLines fileLines = unlines $ filterNonTrees (map parse1Line fileLines)
+
+filterNonTrees = filter (\ln -> ln /= "[]")
+
+treeName :: Parser String
+treeName = do
+			string "'"
+			tname <- many (sat (\c -> c /= '\''))
+			string "'"
+			return tname
+
+treeComment :: Parser String
+treeComment = do
+				string "["
+				tcomm <- many (sat (\c -> c /= '[' && c /= ']'))
+				string "]"
+				return tcomm
+
+treeCommentLik :: Parser Double
+treeCommentLik = do
+					string "[Likelihood of "
+					l <- realnum
+					many (sat (\c -> c /= '[' && c /= ']'))
+					string "]"
+					return l
+
+parseTreeLine :: Parser (Double, String)
+parseTreeLine = do
+					token (string "TREE")
+					treeName
+					space
+					l <- treeCommentLik
+					t <- rubishPlusNewickTree
+					return (l, t)
 
